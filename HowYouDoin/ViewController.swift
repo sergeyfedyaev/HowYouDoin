3da8b2666b171bedf73c32f7db53b2c7ec935d8b
sergeyfedyaev <elserjo2@gmail.com>
2020-08-13 11:08:31 +0300

    Initial Commit

---
+++ HowYouDoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowYouDoin
+//
+//  Created by Сергей Федяев on 13.08.2020.
+//  Copyright © 2020 Sergey Fedyaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
