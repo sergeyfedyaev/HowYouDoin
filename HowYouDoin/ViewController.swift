2ca1d0b2f05b25069ab12f8a5096bacaa731163a
sergeyfedyaev <elserjo2@gmail.com>
2020-08-13 13:30:41 +0300

    Play Sound and Show Image

--- HowYouDoin/ViewController.swift
+++ HowYouDoin/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageLabel: UIImageView!
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageLabel.alpha = 0
     }
-
-
+    
+    @IBAction func sayButtonPressed(_ sender: UIButton) {
+        imageLabel.alpha == 1 
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "audio", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    
 }
 
---
+++ README.md
@@ -0,0 +1,15 @@
+![App by Sergey Fedyaev Banner](Documentation/Banner.png)
+
+# HowYouDoin'?
+
+## About App
+
+This app makes Joey say his famous line when pressing the button.
+
+## What I learned
+
+During the development of this app I learned:
+
+* Working with buttons
+* Working with mp3 files
+* Making the image apper and disappear
